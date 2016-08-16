40f9238da514bd656042eeb9d0bbd12b427586df
yoosan <576948259@qq.com>
2016-08-16 21:41:53 +0800

    update

--- agent.lua
+++ agent.lua
@@ -13,12 +13,15 @@ local agent = torch.class('deeprl.agent')
 
 function agent:__init(config)
     self.max_mem = config.max_mem
-    self.network = self:create_network()
+    self.policy_net = self:create_network()
     self.memory = {}
     self.criterion = nn.MSECriterion()
     self.bsize = config.bsize
     self.n_actions = config.n_actions
     self.n_states = config.n_states
+    self.discount = config.discount
+    self.hid_dim = config.hid_dim
+    self.env = config.env
 end
 
 function agent:remember(mem_input)
@@ -28,11 +31,47 @@ function agent:remember(mem_input)
     end
 end
 
-function agent:gen_batch()
+function agent:generate_batch()
     local mem_size = #self.memory
     local bsize = math.min(mem_size, self.bsize)
+    local indices = torch.randperm(mem_size)
     -- inputs are screens, targets are actions
     local inputs = torch.zeros(bsize, self.n_states)
     local targets = torch.zeros(bsize, self.n_actions)
 
+    for i = 1, bsize do
+        local index = math.random(1, mem_size)
+        local mem_input = self.memory[index]
+
+        local target = self.policy_net:forward(mem_input.input_state):clone()
+
+        local next_target = self.policy_net:forward(mem_input.next_state)
+        local next_state_max_q = torch.max(next_target)
+        if mem_input.game_over then
+            target[mem_input.action] = mem_input.reward
+        else
+            -- reward + discount(gamma) * max_a' Q(s', a')
+            -- expected Q-value of reward + gamma * max a' Q(s', a')
+            target[mem_input.action] = mem_input.reward + self.discount * next_state_max_q
+        end
+
+        -- update the inputs and targets
+        inputs[i] = mem_input.input_state
+        targets[i] = target
+    end
+    return inputs, targets
+end
+
+function agent:create_network()
+    local net = nn.Sequential()
+    net:add(nn.Linear(self.n_states, self.hid_dim))
+    net:add(nn.ReLU())
+    net:add(nn.Linear(self.hid_dim, self.hid_dim))
+    net:add(nn.ReLU())
+    net:add(nn.Linear(self.hid_dim, self.n_actions))
+    return net
+end
+
+function agent:train()
+    
 end
